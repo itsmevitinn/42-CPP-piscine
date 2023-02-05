8e272a0f2a9ed0df33352465e17a35e2796441f7
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-02-05 14:52:32 -0300

    refactor: changed nullptr to null

--- module-01/ex01/zombieHorde.cpp
+++ module-01/ex01/zombieHorde.cpp
@@ -4,7 +4,7 @@
 
 Zombie* zombieHorde(int N, std::string name) {
   if (N <= 0)
-    return (nullptr);
+    return (NULL);
   Zombie* horde = new Zombie[N];
   for (int i = 0; i < N; i++) {
     horde[i].setName(name);
