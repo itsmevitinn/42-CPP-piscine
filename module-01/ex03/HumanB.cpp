501f83b5f4247453697b3bbf5f2426fc470bb3b3
Vitor Sergio <vsergio@c2t1s5.42.rio>
2023-02-06 12:31:09 -0300

    fix: handled when humanB doesn't have a weapon

--- module-01/ex03/HumanB.cpp
+++ module-01/ex03/HumanB.cpp
@@ -4,6 +4,7 @@
 
 HumanB::HumanB(std::string name) {
   this->_name = name;
+  this->_myWeapon = NULL;
 }
 
 void HumanB::setWeapon(Weapon& weaponInstance) {
@@ -15,5 +16,9 @@ std::string HumanB::getName(void) const {
 }
 
 void HumanB::attack(void) const {
-  std::cout << this->getName() << " attacks with their " << this->_myWeapon->getType() << std::endl;
+  if (!this->_myWeapon) {
+    std::cout << this->getName() << " doesn't have a weapon" << std::endl;
+  } else {
+    std::cout << this->getName() << " attacks with their " << this->_myWeapon->getType() << std::endl;
+  }
 }
