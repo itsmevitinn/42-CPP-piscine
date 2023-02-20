e4de6083e3366ff0758cfcfd5a43f6430ab6ae18
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-02-20 20:10:40 -0300

    refactor: using do while in getInput

--- module-00/ex01/main.cpp
+++ module-00/ex01/main.cpp
@@ -1,50 +1,49 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 
-void  getInput(std::string output, std::string &buffer);
+void getInput(std::string output, std::string &buffer);
 
 void createContact(PhoneBook &myPhoneBook) {
   Contact newContact;
   std::string buffer;
 
-  getInput("First name: " ,buffer);
+  getInput("First name: ", buffer);
   newContact.setFirstName(buffer);
 
-  getInput("Last name: " ,buffer);
+  getInput("Last name: ", buffer);
   newContact.setLastName(buffer);
 
-  getInput("Nickname: " ,buffer);
+  getInput("Nickname: ", buffer);
   newContact.setNickname(buffer);
 
-  getInput("Phone number: " ,buffer);
+  getInput("Phone number: ", buffer);
   newContact.setPhoneNumber(buffer);
 
-  getInput("Darkest secret: " ,buffer);
+  getInput("Darkest secret: ", buffer);
   newContact.setDarkestSecret(buffer);
-  
+
   myPhoneBook.addContact(newContact);
 }
 
-void  getInput(std::string output, std::string &buffer){
-  std::cout << output << std::endl << "-> ";
-  std::getline(std::cin, buffer);
-  while(buffer.empty()){
-    std::cout << output << std::endl << "-> ";
+void getInput(std::string output, std::string &buffer) {
+  do {
+    std::cout << output << std::endl;
+    std::cout << "-> ";
     std::getline(std::cin, buffer);
-  }
+  } while (buffer.empty());
 }
 
-void  searchContact(PhoneBook &myPhoneBook){
+void searchContact(PhoneBook &myPhoneBook) {
   std::string contactId;
 
   myPhoneBook.printContacts();
-  std::cout << std::endl << "ID to search a contact: " << std::endl;
+  std::cout << std::endl
+            << "ID to search a contact: " << std::endl;
   std::cout << "-> ";
   std::getline(std::cin, contactId);
-  try{
+  try {
     myPhoneBook.contactInfo(stoi(contactId));
-  }
-  catch (...){
+  } catch (...) {
     std::cout << "Invalid ID!" << std::endl;
   }
 }
@@ -53,9 +52,11 @@ int main(void) {
   PhoneBook myPhoneBook;
   std::string option;
 
-  std::cout << std::endl << "Welcome to your awesome phonebook!" << std::endl;
+  std::cout << std::endl
+            << "Welcome to your awesome phonebook!" << std::endl;
   while (42 && !std::cin.eof()) {
-    std::cout << std::endl << "Choose one of the options below!" << std::endl;
+    std::cout << std::endl
+              << "Choose one of the options below!" << std::endl;
     std::cout << "ADD | SEARCH | EXIT" << std::endl;
     std::cout << "-> ";
     std::getline(std::cin, option);
