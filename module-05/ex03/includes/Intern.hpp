9439e5ea22e9424639a4b091e4593d0950883fbf
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-02-23 20:02:00 -0300

    improve: made makeForm in a cleaner way

--- module-05/ex03/includes/Intern.hpp
+++ module-05/ex03/includes/Intern.hpp
@@ -17,6 +17,12 @@ class Intern {
    public:
     const char* what() const throw();
   };
+  typedef Form* (Intern::*InternForm)(std::string target);
+
+ private:
+  Form* makeShrub(std::string target);
+  Form* makeRobo(std::string target);
+  Form* makePres(std::string target);
 };
 
 #endif
--- module-05/ex03/src/Intern.cpp
+++ module-05/ex03/src/Intern.cpp
@@ -24,13 +24,25 @@ const char* Intern::InvalidForm::what() const throw() {
   return ("Invalid form name!");
 }
 
+Form* Intern::makeShrub(std::string target) {
+  return (new ShrubberyCreationForm(target));
+}
+Form* Intern::makeRobo(std::string target) {
+  return (new RobotomyRequestForm(target));
+}
+Form* Intern::makePres(std::string target) {
+  return (new PresidentialPardonForm(target));
+}
+
 Form* Intern::makeForm(std::string name, std::string target) {
-  Form* sheets[3] = {new ShrubberyCreationForm(target), new RobotomyRequestForm(target), new PresidentialPardonForm(target)};
+  Form* selected;
+  InternForm types[3] = {&Intern::makeShrub, &Intern::makeRobo, &Intern::makePres};
   std::string forms[3] = {"Shrubbery", "Robotomy", "Presidential"};
   for (int i = 0; i < 3; i++) {
     if (name == forms[i]) {
+      selected = (this->*types[i])(target);
       std::cout << "Intern creates " << forms[i] << std::endl;
-      return (sheets[i]);
+      return (selected);
     }
   }
   throw Intern::InvalidForm();
