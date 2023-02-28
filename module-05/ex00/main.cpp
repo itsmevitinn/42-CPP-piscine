21229344313d1db393de683e1afc45fad3a355ec
Vitor Sergio <vsergio@c2t2s1.42.rio>
2023-02-28 13:05:41 -0300

    fix: fixed increase and decrease logic

--- module-05/ex00/main.cpp
+++ module-05/ex00/main.cpp
@@ -3,25 +3,27 @@
 int main(void) {
   try {
     Bureaucrat rat;
-    Bureaucrat politician1("obama", 3);
-    Bureaucrat politician2("joebiden", 148);
+    Bureaucrat politician1("obama", 148);
+    Bureaucrat politician2("joebiden", 3);
     std::cout << rat << std::endl;
+
     politician1.reduceGrade();
     std::cout << politician1 << std::endl;
     politician1.reduceGrade();
     std::cout << politician1 << std::endl;
+
     politician2.increaseGrade();
     std::cout << politician2 << std::endl;
     politician2.increaseGrade();
     std::cout << politician2 << std::endl;
     politician2.increaseGrade();
+
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
   try {
-    Bureaucrat politician1("obama", 0);
-    Bureaucrat politician2("joebiden", 100);
-    std::cout << politician2 << std::endl;
+    Bureaucrat politician1("obama", 151);
+    std::cout << politician1 << std::endl;
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
