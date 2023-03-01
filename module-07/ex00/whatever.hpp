ce26b29528ffafc16b68661af2f7ee3695d8a087
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-03-01 19:50:25 -0300

    refactor: added some comments

--- module-07/ex00/whatever.hpp
+++ module-07/ex00/whatever.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//will receive any type of "x" and "y" and swap each other
 template <typename T>
 void swap(T& x, T& y) {
   T tmp;
@@ -8,13 +9,14 @@ void swap(T& x, T& y) {
   x = y;
   y = tmp;
 }
-
+//will receive any type of "x" and "y" and return the smallest
 template <typename T>
 const T& min(const T& x, const T& y) {
   return (x < y ? x : y);
 }
-
+//will receive any type of "x" and "y" and return the greatest
 template <typename T>
+//return as const T& because we receive as const T&
 const T& max(const T& x, const T& y) {
   return (x > y ? x : y);
 }
