ae202ae58dc7f3376be29950c9609b0ddeb10638
Vitor Sergio Marcondes Costa <vitor@Vitors-MacBook-Air.local>
2023-03-01 12:48:18 -0300

    refactor: added some comments

--- module-07/ex01/iter.hpp
+++ module-07/ex01/iter.hpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
+//here, iter will receive any type of array, iterate through this array and apply the "f" function in each content of the array
 template <typename T>
 void iter(T* array, size_t length, void (*f)(const T&)) {
   for (size_t i = 0; i < length; i++) {
     (*f)(array[i]);
   }
 }
-
+//this function will print any type of value received as parameter
 template <typename T>
 void printArray(const T& value) {
   std::cout << value << std::endl;
--- module-07/ex01/main.cpp
+++ module-07/ex01/main.cpp
@@ -3,12 +3,14 @@
 int main(void) {
   int array[] = {3, 4, 5};
   int length = sizeof(array) / sizeof(array[0]);
+  // implicit instanciation of "printArray"
   ::iter(array, length, &printArray);
 
   std::string names[] = {"Vitor", "Camila", "Gabriel"};
   ::iter(names, 3, &printArray);
 
   float numbers[] = {5.3, 10.3f, -104.22, -40};
-  ::iter(numbers, 4, &printArray);
+  // explicit instanciation of "printArray"
+  ::iter(numbers, 4, &printArray<float>);
   return (0);
 }
