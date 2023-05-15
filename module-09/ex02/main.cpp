e45673cd69561d28c39ebb42160d2a147cecdf5b
Vitor Sergio <vsergio@c2t2s2.42.rio>
2023-05-15 18:47:32 -0300

    refactor: improve readability of code

--- module-09/ex02/main.cpp
+++ module-09/ex02/main.cpp
@@ -10,16 +10,14 @@ int main(int argc, char** argv) {
 
     clock_t start_time = clock();
     mergeSort(vector, 0, vector.size() - 1, 16);
-    clock_t end_time = clock();
-    double duration = (end_time - start_time) * 1000000.0 / CLOCKS_PER_SEC;
+    double duration = (clock() - start_time) * 1000000.0 / CLOCKS_PER_SEC;
 
     printVector(vector, "after");
     std::cout << "Time to process a range of " << vector.size() << " elements with std::vector : " << duration << " us" << std::endl;
 
     start_time = clock();
     mergeSort(deque, 0, deque.size() - 1, 16);
-    end_time = clock();
-    duration = (end_time - start_time) * 1000000.0 / CLOCKS_PER_SEC;
+    duration = (clock() - start_time) * 1000000.0 / CLOCKS_PER_SEC;
 
     std::cout << "Time to process a range of " << deque.size() << " elements with std::deque : " << duration << " us" << std::endl;
 
