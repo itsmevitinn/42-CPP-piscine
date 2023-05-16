efd80f7dbbd2bead3e7cc0c3d3505c1719102451
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-05-16 00:52:33 -0300

    refactor: better functions organization

--- module-09/ex00/BitcoinExchange.hpp
+++ module-09/ex00/BitcoinExchange.hpp
@@ -7,4 +7,10 @@
 #include <map>
 #include <string>
 
+void getDatabaseContent(std::map<std::string, float>& database);
+void getInputContent(std::map<std::string, float>& database, std::string filename);
+int isValidDate(std::string date);
+int isValidValue(float value);
+void printBitcoinValue(std::string date, float value, std::map<std::string, float>& database);
+
 #endif
