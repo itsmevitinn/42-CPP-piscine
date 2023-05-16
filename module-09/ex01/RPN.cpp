cc411fa1dee040cdeb74fcfdcb121a69a1418ebe
Vitor Sergio <vsergio@c2t2s2.42.rio>
2023-05-16 12:15:23 -0300

    feat: add canonical form

--- module-09/ex01/RPN.cpp
+++ module-09/ex01/RPN.cpp
@@ -1,15 +1,32 @@
 #include "RPN.hpp"
 
+// constructors and destructors
+RPN::RPN(void) {
+}
+
+RPN::RPN(const RPN& instance) : _stack(instance._stack) {
+}
+
+RPN::~RPN(void) {
+}
+
+// operator overload
+RPN& RPN::operator=(const RPN& instance) {
+  if (this != &instance)
+    this->_stack = instance._stack;
+  return (*this);
+}
+
 void RPN::addNumber(int number) {
-  this->stack.push(number);
+  this->_stack.push(number);
 }
 
 void RPN::calculate(Operator type) {
-  if (this->stack.size() >= 2) {
-    int first_operand = this->stack.top();
-    this->stack.pop();
-    int second_operand = this->stack.top();
-    this->stack.pop();
+  if (this->_stack.size() >= 2) {
+    int first_operand = this->_stack.top();
+    this->_stack.pop();
+    int second_operand = this->_stack.top();
+    this->_stack.pop();
     int result;
     switch (type) {
       case sum:
@@ -25,12 +42,12 @@ void RPN::calculate(Operator type) {
         result = second_operand * first_operand;
         break;
     }
-    this->stack.push(result);
+    this->_stack.push(result);
   }
 }
 
 void RPN::printStack(void) {
-  if (this->stack.size() != 1)
+  if (this->_stack.size() != 1)
     throw std::invalid_argument("Impossible to perform reverse polish notation");
-  std::cout << this->stack.top() << std::endl;
+  std::cout << this->_stack.top() << std::endl;
 }
--- module-09/ex01/RPN.hpp
+++ module-09/ex01/RPN.hpp
@@ -8,13 +8,19 @@
 enum Operator { sum, subtract, divide, multiply };
 
 class RPN {
-   private:
-    std::stack<int> stack;
+ private:
+  std::stack<int> _stack;
 
-   public:
-    void addNumber(int number);
-    void calculate(Operator type);
-    void printStack(void);
+ public:
+  // constructors and destructors
+  RPN(void);
+  RPN(const RPN& instance);
+  ~RPN(void);
+  // copy assignment operator overload
+  RPN& operator=(const RPN& instance);
+  void addNumber(int number);
+  void calculate(Operator type);
+  void printStack(void);
 };
 
 #endif
